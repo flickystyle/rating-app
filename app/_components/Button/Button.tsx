d7bbbeb590aedb771af8bfd9c7d9f26a1e000df6
Dmitry <superflick3@gmail.com>
2025-08-01 12:53:48 +0300

    Add dependences, build some components, initial project

---
+++ app/_components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { JSX } from 'react';
+import { ButtonProps } from './Button.props';
+import styles from './Button.module.css';
+import cn from 'classnames';
+
+export default function Button({
+    children,
+    appearance,
+    className,
+    ...props
+}: ButtonProps): JSX.Element {
+    return (
+        <button
+            className={cn(styles.button, className, {
+                [styles.primary]: appearance === 'primary',
+                [styles.ghost]: appearance === 'ghost',
+            })}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
